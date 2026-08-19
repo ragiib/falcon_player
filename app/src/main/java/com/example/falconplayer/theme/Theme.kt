27c54938cf801edffc77f5d5a42abbe7c894ba01
Ragib <ragibasif7@gmail.com>
2026-08-19 22:23:37 +0530

    cleaning code

--- app/src/main/java/com/example/falconplayer/theme/Theme.kt
+++ app/src/main/java/com/example/falconplayer/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.falconplayer.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
